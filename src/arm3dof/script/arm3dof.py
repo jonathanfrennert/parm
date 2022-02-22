0622a7d8e1adb3df71ff2acfcecc9ac8acfdd614
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2022-02-21 16:06:15 -0800

    world state done + some of state

--- src/arm3dof/script/arm3dof.py
+++ src/arm3dof/script/arm3dof.py
@@ -16,24 +16,98 @@ from sklearn.neighbors import KDTree
 #   World
 #   List of objects, start, goal, and parameters.
 #
-(l1, l2, l3)             = (0.0, 0.5, 0.5)              # arm link lengths
-(t1, t2, t3)             = (0, 0, 0)                    # initial arm joint angles
+ls = np.array([0.0, 1.0, 1.0]) # arm link lengths
+L  = ls.sum()
 
-R = l2 + l3
 
 # Construct the walls (boxes) (x, y, z, roll, pitch, yaw, length, width, height).
-walls = [ [0, 0, 0, ]
-]
+floor      = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2*L, 2*L, 0.0])
+ceiling    = np.array([0.0, 0.0, L, 0.0, 0.0, 0.0, 2*L, 2*L, 0.0])
+x_pos_wall = np.array([L, 0.0, L/2, 0.0, 0.0, 0.0, 0.0, 2*L, L])
+x_neg_wall = np.array([-L, 0.0, L/2, 0.0, 0.0, 0.0, 0.0, 2*L, L])
+y_pos_wall = np.array([0, L, L/2, 0.0, 0.0, 0.0, 2*L, 0.0, L])
+y_pos_wall = np.array([0, -L, L/2, 0.0, 0.0, 0.0, 2*L, 0.0, L])
 
-# Pick your start and goal locations.
-(startx, starty, startt) = (2.0, 2.0, 0.0)
-(goalx,  goaly,  goalt)  = (xspace + (lspace-lcar)/2 + lb, wroad + wc, 0.0)
+obstacles = [floor, ceiling,
+             x_pos_wall, x_neg_wall,
+             y_pos_wall, y_neg_wall]
 
-# Spacing for drawing/testing
-ddraw = 0.5
+
+# Pick your start and goal locations (in radians).
+startts = np.array([0.0, 0.0, 0.0])
+goalts  = np.array([1.16, 2.36, -1.49])
+
 
 # PRM parameters
-#N = 800     # 200
-#K = 50      # 40
-N = 200
-K = 40
+N = 800
+K = 50
+
+
+# RRT parameters
+dstep = 0.25
+Nmax  = 1000
+
+
+######################################################################
+#
+#   State Definition
+#
+class State:
+    def __init__(self, ts):
+        # Pre-compute the trigonometry.
+        self.ts = ts
+        self.cs = np.cos(thetas)
+        self.ss = np.sin(thetas)
+        self.ps = fkin(ts, cs, ss)
+
+
+    ############################################################
+    # Utilities:
+    # In case we want to print the state.
+    def __repr__(self):
+        repr_str = '< - '
+        for i, t in enumerate(ts):
+            repr_str += f'T{i} {t * (180/np.pi):5.1f} deg - '
+        return return repr_str + '>'
+
+    # Draw where the state is:
+    def Draw(self, *args, **kwargs):
+        plt.plot(self.x, self.y, *args, **kwargs)
+        plt.pause(0.001)
+
+
+    ############################################################
+    # PRM Functions:
+    # Check whether in free space.
+    def InFreeSpace(self):
+        for wall in walls:
+            if SegmentCrossBox(wall, self.box):
+                return False
+        return True
+
+    # Compute the relative distance to another state.  Scale the
+    # angular error by the car length.
+    def Distance(self, other):
+        return np.sqrt((self.x - other.x)**2 +
+                       (self.y - other.y)**2 +
+                       (wheelbase*AngleDiff(self.t, other.t))**2)
+
+
+    ############################################################
+    # RRT Functions:
+    # Compute the relative distance to another state.
+    def DistSquared(self, other):
+        return ((self.x - other.x)**2 + (self.y - other.y)**2)
+
+    # Compute/create an intermediate state.
+    def Intermediate(self, other, alpha):
+        return State(self.x + alpha * (other.x - self.x),
+                     self.y + alpha * (other.y - self.y))
+
+    # Check the local planner - whether this connects to another state.
+    def ConnectsTo(self, other):
+        for triangle in triangles:
+            if SegmentCrossTriangle(((self.x, self.y), (other.x, other.y)),
+                                    triangle):
+                return False
+        return True
