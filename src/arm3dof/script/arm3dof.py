fbc05a5b1898e692574b8bf89e8bf8255a39eec6
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2022-02-22 13:00:20 -0800

    vector fkin

--- src/arm3dof/script/arm3dof.py
+++ src/arm3dof/script/arm3dof.py
@@ -26,7 +26,7 @@ ceiling    = np.array([0.0, 0.0, L, 0.0, 0.0, 0.0, 2*L, 2*L, 0.0])
 x_pos_wall = np.array([L, 0.0, L/2, 0.0, 0.0, 0.0, 0.0, 2*L, L])
 x_neg_wall = np.array([-L, 0.0, L/2, 0.0, 0.0, 0.0, 0.0, 2*L, L])
 y_pos_wall = np.array([0, L, L/2, 0.0, 0.0, 0.0, 2*L, 0.0, L])
-y_pos_wall = np.array([0, -L, L/2, 0.0, 0.0, 0.0, 2*L, 0.0, L])
+y_neg_wall = np.array([0, -L, L/2, 0.0, 0.0, 0.0, 2*L, 0.0, L])
 
 obstacles = [floor, ceiling,
              x_pos_wall, x_neg_wall,
@@ -38,12 +38,12 @@ startts = np.array([0.0, 0.0, 0.0])
 goalts  = np.array([1.16, 2.36, -1.49])
 
 
-# PRM parameters
+# PRM default parameters
 N = 800
 K = 50
 
 
-# RRT parameters
+# RRT default parameters
 dstep = 0.25
 Nmax  = 1000
 
@@ -56,8 +56,8 @@ class State:
     def __init__(self, ts):
         # Pre-compute the trigonometry.
         self.ts = ts
-        self.cs = np.cos(thetas)
-        self.ss = np.sin(thetas)
+        self.cs = np.cos(ts)
+        self.ss = np.sin(ts)
         self.ps = fkin(ts, cs, ss)
 
 
@@ -66,48 +66,124 @@ class State:
     # In case we want to print the state.
     def __repr__(self):
         repr_str = '< - '
-        for i, t in enumerate(ts):
+        for i, t in enumerate(self.ts):
             repr_str += f'T{i} {t * (180/np.pi):5.1f} deg - '
-        return return repr_str + '>'
+        return repr_str + '>'
 
     # Draw where the state is:
-    def Draw(self, *args, **kwargs):
-        plt.plot(self.x, self.y, *args, **kwargs)
-        plt.pause(0.001)
+    #def Draw(self, *args, **kwargs):
+    #    plt.plot(self.x, self.y, *args, **kwargs)
+    #    plt.pause(0.001)
 
 
     ############################################################
     # PRM Functions:
     # Check whether in free space.
-    def InFreeSpace(self):
-        for wall in walls:
-            if SegmentCrossBox(wall, self.box):
-                return False
-        return True
+    #def InFreeSpace(self):
+    #    for wall in walls:
+    #        if SegmentCrossBox(wall, self.box):
+    #            return False
+    #    return True
 
     # Compute the relative distance to another state.  Scale the
     # angular error by the car length.
-    def Distance(self, other):
-        return np.sqrt((self.x - other.x)**2 +
-                       (self.y - other.y)**2 +
-                       (wheelbase*AngleDiff(self.t, other.t))**2)
+    #def Distance(self, other):
+    #    return np.sqrt((self.x - other.x)**2 +
+    #                   (self.y - other.y)**2 +
+    #                   (wheelbase*AngleDiff(self.t, other.t))**2)
 
 
     ############################################################
     # RRT Functions:
     # Compute the relative distance to another state.
-    def DistSquared(self, other):
-        return ((self.x - other.x)**2 + (self.y - other.y)**2)
+    #def DistSquared(self, other):
+    #    return ((self.x - other.x)**2 + (self.y - other.y)**2)
 
     # Compute/create an intermediate state.
-    def Intermediate(self, other, alpha):
-        return State(self.x + alpha * (other.x - self.x),
-                     self.y + alpha * (other.y - self.y))
+    #def Intermediate(self, other, alpha):
+    #    return State(self.x + alpha * (other.x - self.x),
+    #                 self.y + alpha * (other.y - self.y))
 
     # Check the local planner - whether this connects to another state.
-    def ConnectsTo(self, other):
-        for triangle in triangles:
-            if SegmentCrossTriangle(((self.x, self.y), (other.x, other.y)),
-                                    triangle):
-                return False
-        return True
+    #def ConnectsTo(self, other):
+    #    for triangle in triangles:
+    #        if SegmentCrossTriangle(((self.x, self.y), (other.x, other.y)),
+    #                                triangle):
+    #            return False
+    #    return True
+
+
+def main():
+    # Report the parameters.
+    print('Running with ', N, ' nodes and ', K, ' neighbors.')
+
+
+    # Create the start/goal nodes.
+    #startnode = Node(State(startts))
+    #goalNode  = Node(State(goalts))
+    print(State(startts))
+    print(State(goalts))
+
+    # Show the start/goal states.
+    #startnode.state.Draw(fig, 'r', linewidth=2)
+    #goalnode.state.Draw(fig,  'r', linewidth=2)
+    #fig.ShowFigure()
+    #input("Showing basic world (hit return to continue)")
+
+
+    # Create the list of sample points.
+    #start = time.time()
+    nodeList = []
+    #AddNodesToListObj(nodeList, N)
+    #print('Sampling took ', time.time() - start)
+
+    # # Show the sample states.
+    #for node in nodeList:
+    #    node.state.Draw(fig, 'k', linewidth=1)
+    #fig.ShowFigure()
+    #input("Showing the nodes (hit return to continue)")
+
+    # Add the start/goal nodes.
+    #nodeList.append(startnode)
+    #nodeList.append(goalnode)
+
+
+    # Connect to the nearest neighbors.
+    #start = time.time()
+    #ConnectNearestNeighbors(nodeList, K)
+    #print('Connecting took ', time.time() - start)
+
+    # # Show the neighbor connections.
+    # for node in nodeList:
+    #     for child in node.children:
+    #         plan = LocalPlan(node.state, child.state)
+    #         plan.Draw(fig, 'g-', linewidth=0.5)
+    # fig.ShowFigure()
+    # input("Showing the full graph (hit return to continue)")
+
+
+    # Run the A* planner.
+    #start = time.time()
+    #path = AStar(nodeList, startnode, goalnode)
+    #print('A* took ', time.time() - start)
+    #if not path:
+    #    print("UNABLE TO FIND A PATH")
+    #    return
+
+
+    # Show the path.
+    #DrawPath(path, fig, 'r', linewidth=1)
+    #fig.ShowFigure()
+    #input("Showing the raw path (hit return to continue)")
+
+    # Post Process the path.
+    #path = PostProcess(path)
+
+    # Show the post-processed path.
+    #DrawPath(path, fig, 'b', linewidth=2)
+    #fig.ShowFigure()
+    #input("Showing the post-processed path (hit return to continue)")
+
+
+if __name__== "__main__":
+    main()
--- src/arm3dof/script/fkin.py
+++ src/arm3dof/script/fkin.py
@@ -1,28 +1,22 @@
 import numpy as np
 
-def fkin(theta, length):
+def fkin(ls, ts, cs, ss):
     """
     Give forward kinematics of each join
     """
-    # Get length
-    l1, l2, l3 = length[0], length[1], length[2]
-
-    # Get angles
-    theta1, theta2, theta3 = theta[0], theta[1], theta[2]
-
     # TODO: Change in future
-    p1 = np.array([0, 0, l1])
+    p1 = np.array([0, 0, ls[0]])
 
     # Get r, z
-    r = l2 * np.cos(theta2)
-    z = l2 * np.sin(theta2)
-    p2 = np.array([- r * np.sin(theta1), r * np.cos(theta1) , z])
+    r = ls[1] * cs[1]
+    z = ls[1] * ss[1]
+    p2 = np.array([- r * ss[0], r * cs[0], z])
 
     # Get r, l for third bar
-    r += l3 * np.cos(theta2 + theta3)
-    z += l3 * np.sin(theta2 + theta3)
+    r += ls[2] * np.cos(ts[1] + ts[2])
+    z += ls[2] * np.sin(ts[1].+ ts[2])
 
     # End effector
-    p3 = np.array([-r * np.sin(theta1), r * np.cos(theta1), z])
+    p3 = np.array([- r * ss[0], r * cs[0], z])
 
-    return [p1, p2, p3]
+    return np.array([p1, p2, p3])
