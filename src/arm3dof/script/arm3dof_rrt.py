83b64fec7fd47fd889e07903978fd09fa010bfd3
Advaith Sai <advaith.maddipatla@gmail.com>
2022-03-05 11:42:11 -0800

    rrt implemented - path shows

--- src/arm3dof/script/arm3dof_rrt.py
+++ src/arm3dof/script/arm3dof_rrt.py
@@ -40,10 +40,10 @@ triangles = ((( 2, 6), ( 3, 2), ( 4, 6)),
 # Number of checks with intermediate states for ConnectsTo
 MAX_CHECKS = 20;
 
-dstep = 0.25
+dstep = 1
 #dstep = 0.5
 #dstep = 1
-dstep = 10
+# dstep = 10
 Nmax  = 1000
 
 # arm link lengths
@@ -59,6 +59,9 @@ sphere2 = np.array([-1.0, 0.0, 1.0, 0.6])
 sphere3 = np.array([0.0, 1.0, 1.0, 0.6])
 sphere4 = np.array([0.0, -1.0, 1.0, 0.6])
 obstacles = [sphere1, sphere2, sphere3, sphere4]
+
+startts = np.array([0.0, 0.0, 0.0])
+goalts  = np.array([0, np.pi / 2, 0])
 
 
 ######################################################################
@@ -132,7 +135,7 @@ class State:
     def ConnectsTo(self, other):
         n = len(self.ts);
         d = self.Distance(other)
-        numConnects = np.floor((MAX_CHECKS * d / (2 * np.pi * np.sqrt(n))))
+        numConnects = int(np.ceil((MAX_CHECKS * d / (2 * np.pi * np.sqrt(n)))))
 
         for alpha in range(1, numConnects + 2):
             intermediate = self.Intermediate(other, alpha / numConnects)
@@ -166,21 +169,25 @@ def RRT(tree, goalstate, Nmax):
     # Loop.
     while True:
         # Determine the target state.
-        x = random.uniform(xmin, xmax)
-        y = random.uniform(ymin, ymax)
-        targetstate = State(x,y)        # How to pick x/y?
+        theta1 = random.uniform(amin, amax)
+        theta2 = random.uniform(amin, amax)
+        theta3 = random.uniform(amin, amax)
+        targetstate = State(np.array([theta1, theta2, theta3]))        # How to pick x/y?
 
         # Find the nearest node (node with state nearest the target state).
         # This is inefficient (slow for large trees), but simple.
-        list = [(node.state.Distance(targetstate), node) for node in tree]
-        (d2, nearnode)  = min(list)
+        list_vals = [(node.state.Distance(targetstate), node) for node in tree]
+        (d2, nearnode)  = min(list_vals)
         d = np.sqrt(d2)
         nearstate = nearnode.state
 
         # Determine the next state, a step size (dstep) away.
-        dx = targetstate.x - nearstate.x
-        dy = targetstate.y - nearstate.y
-        nextstate = State(nearstate.x + dstep * dx,  nearstate.y  + dstep * dy)
+        d_theta1 = targetstate.ts[0] - nearstate.ts[0]
+        d_theta2 = targetstate.ts[1] - nearstate.ts[1]
+        d_theta3 = targetstate.ts[2] - nearstate.ts[2]
+        nextstate = State(np.array([nearstate.ts[0] + dstep * d_theta1,
+                                    nearstate.ts[1] + dstep * d_theta2,
+                                    nearstate.ts[2] + dstep * d_theta3]))
 
         # Check whether to attach (creating a new node).
         if nearstate.ConnectsTo(nextstate):
@@ -202,24 +209,28 @@ def targetRRT(tree, goalstate, Nmax):
     # Loop.
     while True:
         # Determine the target state.
-        if random.uniform(0,1) < 0.05:
+        if random.uniform(0,1) < 0.1:
             targetstate = goalstate
         else:
-            x = random.uniform(xmin, xmax)
-            y = random.uniform(ymin, ymax)
-            targetstate = State(x,y)
+            theta1 = random.uniform(amin, amax)
+            theta2 = random.uniform(amin, amax)
+            theta3 = random.uniform(amin, amax)
+            targetstate = State(np.array([theta1, theta2, theta3]))
 
         # Find the nearest node (node with state nearest the target state).
         # This is inefficient (slow for large trees), but simple.
-        list = [(node.state.Distance(targetstate), node) for node in tree]
-        (d2, nearnode)  = min(list)
+        list_vals = [(node.state.Distance(targetstate), node) for node in tree]
+        (d2, nearnode)  = min(list_vals)
         d = np.sqrt(d2)
         nearstate = nearnode.state
 
         # Determine the next state, a step size (dstep) away.
-        dx = targetstate.x - nearstate.x
-        dy = targetstate.y - nearstate.y
-        nextstate = State(nearstate.x + dstep * dx,  nearstate.y  + dstep * dy)
+        d_theta1 = targetstate.ts[0] - nearstate.ts[0]
+        d_theta2 = targetstate.ts[1] - nearstate.ts[1]
+        d_theta3 = targetstate.ts[2] - nearstate.ts[2]
+        nextstate = State(np.array([nearstate.ts[0] + dstep * d_theta1,
+                                    nearstate.ts[1] + dstep * d_theta2,
+                                    nearstate.ts[2] + dstep * d_theta3]))
 
         # Check whether to attach (creating a new node).
         if nearstate.ConnectsTo(nextstate):
@@ -244,20 +255,10 @@ def main():
     # Report the parameters.
     print('Running with step size ', dstep, ' and up to ', Nmax, ' nodes.')
 
-    # Create the figure.
-    Visual = Visualization()
-
 
     # Set up the start/goal states.
-    startstate = State(startx, starty)
-    goalstate  = State(goalx,  goaly)
-
-    # Show the start/goal states.
-    startstate.Draw('ro')
-    goalstate.Draw('ro')
-    Visual.ShowFigure()
-    input("Showing basic world (hit return to continue)")
-
+    startstate = State(startts)
+    goalstate  = State(goalts)
 
     # Start the tree with the start state and no parent.
     tree = [Node(startstate, None)]
@@ -272,13 +273,15 @@ def main():
         return
 
     # Show the path.
+    paths = []
     while node.parent is not None:
-        node.Draw('b-', linewidth=2)
+        paths.append(node.parent.state)
         node = node.parent
-    print("PATH found after %d samples", len(tree))
-    input("(hit return to exit)")
-    return
 
+    paths = paths[::-1]
+    for val in paths:
+        print(val)
+    return paths
 
 if __name__== "__main__":
     main()
